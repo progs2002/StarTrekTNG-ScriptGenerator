7a580ea10fc2b02446b7824677d85fa18b421daf
progs <pragatobhattacharjee@gmail.com>
2024-01-17 21:07:12 +0530

    minimized processing, keeping all dialogue cues and scene info

--- process_data.py
+++ process_data.py
@@ -9,38 +9,14 @@ def clean_text(text):
     idx1 = list(re.finditer(r'FADE IN:', text))[0].span()[1]
     text = text[idx1:]
 
-    #remove cinematic info
-    cap2 = re.compile(r'^\d.*\n', re.MULTILINE)
+    cap2 = re.compile(r'^\d{1,}(.*)\n', re.MULTILINE)
     cap3 = re.compile(r'^.*STAR TREK.*$', re.MULTILINE)
-    text = cap2.sub('',text)
+    text = cap2.sub(r'\1:',text)
     text = cap3.sub('',text)
 
     #CHARACTER_NAME -> CHARACTER_NAME:
     cap1 = re.compile(r'^\t{5}([^\t\n]+)$', re.MULTILINE)
     text = cap1.sub(r'\1:',text)
-
-    #remove dialogue cues 
-    cap4 =  re.compile(r'^\t{4}\(.*\n*.*\)\n', re.MULTILINE)
-    text = cap4.sub('',text)
-
-    #remove extra lines
-    cap5 = re.compile(r'^.*FADE IN:.*\n', re.MULTILINE)
-    cap6 = re.compile(r'^.*FADE OUT.*\n', re.MULTILINE)
-    cap7 = re.compile(r'^.*PART.*$', re.MULTILINE)
-    cap8 = re.compile(r'^.*ACT.*$', re.MULTILINE)
-    cap9 = re.compile(r'^\s*\(\w+\).*$', re.MULTILINE)
-    cap10 = re.compile(r'^.*TEASER.*$', re.MULTILINE)
-    cap11 = re.compile(r'^thru.*$', re.MULTILINE)
-    cap12 = re.compile(r'^\t{9}CUT.*$', re.MULTILINE)
-
-    text = cap5.sub('',text)
-    text = cap6.sub('',text)
-    text = cap7.sub('',text)
-    text = cap8.sub('',text)
-    text = cap9.sub('',text)
-    text = cap10.sub('',text)
-    text = cap11.sub('',text)
-    text = cap12.sub('',text)
 
     text = text.strip()
     
